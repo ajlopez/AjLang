4f67775c78c3f5ee93f43b23e787314b5a05cc85
ajlopez <webmaster@ajlopez.com>
2011-12-16 11:09:56 -0300

    Environment with first tests

---
+++ Src/AjLang.Tests/EnvironmentTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AjLang.Tests
+{
+    [TestClass]
+    public class EnvironmentTests
+    {
+        [TestMethod]
+        public void GetUndefinedValue()
+        {
+            Environment environment = new Environment();
+            Assert.IsNull(environment.GetValue("Foo"));
+        }
+
+        [TestMethod]
+        public void SetAndGetValue()
+        {
+            Environment environment = new Environment();
+            environment.SetValue("One", 1);
+            Assert.AreEqual(1, environment.GetValue("One"));
+        }
+    }
+}
+
---
+++ Src/AjLang/Environment.cs
@@ -0,0 +1,25 @@
+﻿namespace AjLang
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Environment
+    {
+        private Dictionary<string, object> values = new Dictionary<string, object>();
+
+        public void SetValue(string name, object value)
+        {
+            this.values[name] = value;
+        }
+
+        public object GetValue(string name)
+        {
+            if (!this.values.ContainsKey(name))
+                return null;
+            
+            return this.values[name];
+        }
+    }
+}
