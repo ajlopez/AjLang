bf439519e53c677d4d84d3f4f0885a677e38ffb3
ajlopez <webmaster@ajlopez.com>
2011-12-16 11:01:51 -0300

    Initial commit; ConstantExpression with first test

---
+++ Src/AjLang.Tests/Expressions/ConstantExpressionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjLang.Expressions;
+
+namespace AjLang.Tests.Expressions
+{
+    [TestClass]
+    public class ConstantExpressionTests
+    {
+        [TestMethod]
+        public void CreateAndEvaluateInteger()
+        {
+            ConstantExpression expr = new ConstantExpression(1);
+
+            Assert.AreEqual(1, expr.Evaluate());
+        }
+    }
+}
---
+++ Src/AjLang/Expressions/ConstantExpression.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AjLang.Expressions
+{
+    public class ConstantExpression
+    {
+        private object value;
+
+        public ConstantExpression(object value)
+        {
+            this.value = value;
+        }
+
+        public object Evaluate()
+        {
+            return this.value;
+        }
+    }
+}
