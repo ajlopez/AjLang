55441f66e252eeac964f7213809a00668dd3ebce
ajlopez <webmaster@ajlopez.com>
2012-01-22 17:24:16 -0300

    DefineCommand with simple test

---
+++ Src/AjLang.Tests/Commands/DefineCommandTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjLang.Commands;
+using AjLang.Expressions;
+
+namespace AjLang.Tests.Commands
+{
+    [TestClass]
+    public class DefineCommandTests
+    {
+        [TestMethod]
+        public void DefineCommand()
+        {
+            IList<ICommand> commands = new List<ICommand>();
+
+            commands.Add(new SetVariableCommand("a", new ConstantExpression(1)));
+            commands.Add(new SetVariableCommand("b", new ConstantExpression(2)));
+
+            DefineCommand command = new DefineCommand("foo", commands);
+
+            Assert.AreEqual("foo", command.Name);
+            Assert.AreEqual(commands, command.Commands);
+        }
+    }
+}
---
+++ Src/AjLang/Commands/DefineCommand.cs
@@ -0,0 +1,28 @@
+﻿namespace AjLang.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class DefineCommand : ICommand
+    {
+        private string name;
+        private IList<ICommand> commands;
+
+        public DefineCommand(string name, IList<ICommand> commands)
+        {
+            this.name = name;
+            this.commands = commands;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public IEnumerable<ICommand> Commands { get { return this.commands; } }
+
+        public void Execute(Context context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
