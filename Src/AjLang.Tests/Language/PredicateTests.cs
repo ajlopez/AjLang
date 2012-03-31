2409411cf122db6a1949f4ad574599d2e3f1dc4f
ajlopez <webmaster@ajlopez.com>
2012-03-31 08:47:51 -0300

    Predicates IsFalse, IsTrue with tests

---
+++ Src/AjLang.Tests/Language/PredicateTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjLang.Language;
+
+namespace AjLang.Tests.Language
+{
+    [TestClass]
+    public class PredicateTests
+    {
+        [TestMethod]
+        public void IsFalse()
+        {
+            Assert.IsTrue(Predicates.IsFalse(null));
+            Assert.IsTrue(Predicates.IsFalse(false));
+            Assert.IsFalse(Predicates.IsFalse(true));
+            Assert.IsFalse(Predicates.IsFalse(string.Empty));
+            Assert.IsFalse(Predicates.IsFalse(0));
+            Assert.IsFalse(Predicates.IsFalse("foo"));
+            Assert.IsFalse(Predicates.IsFalse(10));
+        }
+
+        [TestMethod]
+        public void IsTrue()
+        {
+            Assert.IsFalse(Predicates.IsTrue(null));
+            Assert.IsFalse(Predicates.IsTrue(false));
+            Assert.IsTrue(Predicates.IsTrue(true));
+            Assert.IsTrue(Predicates.IsTrue(string.Empty));
+            Assert.IsTrue(Predicates.IsTrue(0));
+            Assert.IsTrue(Predicates.IsTrue("foo"));
+            Assert.IsTrue(Predicates.IsTrue(10));
+        }
+    }
+}
---
+++ Src/AjLang/Language/Predicates.cs
@@ -0,0 +1,26 @@
+﻿namespace AjLang.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Predicates
+    {
+        public static bool IsFalse(object obj)
+        {
+            if (obj == null)
+                return true;
+            if (obj is bool && ((bool)obj) == false)
+                return true;
+
+            return false;
+        }
+
+        public static bool IsTrue(object obj)
+        {
+            return !IsFalse(obj);
+        }
+    }
+}
+
